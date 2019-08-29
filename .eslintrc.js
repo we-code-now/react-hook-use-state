1a7f918445942fa6c91359ad9ae08bcde5ea8630
StevenTea <5902761+StevenTea@users.noreply.github.com>
2019-08-29 15:52:07 +0700

    Update eslint rules for react hooks.

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,12 +17,22 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', 'react-hooks'],
   rules: {
     'arrow-parens': ['error', 'as-needed'],
     'brace-style': ['error', 'stroustrup'],
-    'object-curly-newline': ['error', { multiline: true, minProperties: 3 }],
+    'object-curly-newline': ['error', { multiline: true, minProperties: 4 }],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        peerDependencies: true,
+        optionalDependencies: false,
+        devDependencies: ['**/*.spec.js', '**/*.test.js'],
+      },
+    ],
     'react/prop-types': ['off'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
   },
 };
